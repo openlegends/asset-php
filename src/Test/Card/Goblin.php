0d46cf1175af56f214314db245e6340846115fbd
openlegends <openlegends@bot>
2024-03-15 15:29:08 +0200

    update var name

--- src/Test/Card/Goblin.php
+++ src/Test/Card/Goblin.php
@@ -7,6 +7,6 @@ namespace OpenLegends\Asset\Test\Card;
 class Goblin extends \OpenLegends\Engine\Abstract\Card
 {
     public function act(
-        \OpenLegends\Engine\Abstract\Card $card
+        \OpenLegends\Engine\Abstract\Card $target
     ) {}
 }
