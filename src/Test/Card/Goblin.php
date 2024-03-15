51ed6aca12b7032a700a32fef0f6a3ef59375c13
openlegends <openlegends@bot>
2024-03-15 15:25:05 +0200

    init test cards preset

---
+++ src/Test/Card/Goblin.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenLegends\Asset\Test\Card;
+
+class Goblin extends \OpenLegends\Engine\Abstract\Card
+{
+    public function act(
+        \OpenLegends\Engine\Abstract\Card $card
+    ) {}
+}
