f154dbd08b5b4fbc701136e6a6aa421ee1f78cf0
openlegends <openlegends@bot>
2024-03-16 23:59:38 +0200

    update card pool namespace

--- src/Test/Game/Pool.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace OpenLegends\Asset\Test\Game;
-
-class Pool extends \OpenLegends\Engine\Abstract\Card\Pool
-{}
---
+++ src/Test/Pool/Card.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenLegends\Asset\Test\Pool;
+
+class Card extends \OpenLegends\Engine\Abstract\Pool\Card
+{}
