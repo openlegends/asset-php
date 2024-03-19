feb357e1a6126c4b7af7c2a554c01579974e18de
openlegends <openlegends@bot>
2024-03-19 18:29:47 +0200

    update engine api

--- src/Test/Game/Arena.php
+++ src/Test/Game/Arena.php
@@ -8,45 +8,12 @@ class Arena extends \OpenLegends\Engine\Abstract\Game
 {
     public function __construct()
     {
-        $deck = new \OpenLegends\Asset\Test\Pool\Card();
-
-        for ($i = 0; $i < 3; $i++) // generate 3 copies of each card
-        {
-            $deck->addCard(
-                new \OpenLegends\Asset\Test\Card\Goblin()
-            );
-
-            $deck->addCard(
-                new \OpenLegends\Asset\Test\Card\Mouse()
-            );
-
-            $deck->addCard(
-                new \OpenLegends\Asset\Test\Card\Rat()
-            );
-        }
-
-        $this->setDeck(
-            $deck
+        $this->addPlayer(
+            new \OpenLegends\Asset\Test\Game\Player\Player1()
         );
 
-        $this->setHand(
-            new \OpenLegends\Asset\Test\Pool\Card()
-        );
-
-        $this->setPile(
-            new \OpenLegends\Asset\Test\Pool\Card()
-        );
-
-        $this->setLeft(
-            new \OpenLegends\Asset\Test\Pool\Card()
-        );
-
-        $this->setRight(
-            new \OpenLegends\Asset\Test\Pool\Card()
-        );
-
-        $this->setPlayer(
-            new \OpenLegends\Asset\Test\Player\Elf()
+        $this->addPlayer(
+            new \OpenLegends\Asset\Test\Game\Player\Player2()
         );
     }
 }
---
+++ src/Test/Game/Player/Player1.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenLegends\Asset\Test\Game\Player;
+
+class Player1 extends \OpenLegends\Engine\Abstract\Game\Player
+{
+    public function __construct()
+    {
+        $this->setHealth(
+            30
+        );
+
+        $deck = new \OpenLegends\Asset\Test\Pool\Card();
+
+        for ($i = 0; $i < 3; $i++) // generate 3 copies of each card
+        {
+            $deck->addCard(
+                new \OpenLegends\Asset\Test\Card\Goblin()
+            );
+
+            $deck->addCard(
+                new \OpenLegends\Asset\Test\Card\Mouse()
+            );
+
+            $deck->addCard(
+                new \OpenLegends\Asset\Test\Card\Rat()
+            );
+        }
+
+        $this->setDeck(
+            $deck
+        );
+
+        $this->setHand(
+            new \OpenLegends\Asset\Test\Pool\Card()
+        );
+
+        $this->setPile(
+            new \OpenLegends\Asset\Test\Pool\Card()
+        );
+
+        $this->setLeft(
+            new \OpenLegends\Asset\Test\Pool\Card()
+        );
+
+        $this->setRight(
+            new \OpenLegends\Asset\Test\Pool\Card()
+        );
+    }
+}
